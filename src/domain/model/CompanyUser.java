1ade69848c77b3cd4b6e9a5ccb0a151d56741afa
Andrzej <bazylek11@gmail.com>
2026-04-23 15:14:38 +0200

    feat: CompanyUser class created

---
+++ src/domain/model/CompanyUser.java
@@ -0,0 +1,12 @@
+package domain.model;
+
+public class CompanyUser extends User {
+    private final String companyName;
+    private final String taxId;
+
+    public CompanyUser(String email, String displayName, String companyName, String taxId) {
+        super(email, displayName);
+        this.companyName = companyName;
+        this.taxId = taxId;
+    }
+}
