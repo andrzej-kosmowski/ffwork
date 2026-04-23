2a10b67365340a52be1dc181175f36eab9742b7a
Andrzej <bazylek11@gmail.com>
2026-04-23 15:20:35 +0200

    feat: abstract Resource class created

---
+++ src/domain/model/Resource.java
@@ -0,0 +1,34 @@
+package domain.model;
+
+import domain.value.Money;
+
+public abstract class Resource {
+    private final String name;
+    private final Money customHourlyRate;
+
+    public Resource(String name) {
+        this.name = name;
+        this.customHourlyRate = null;
+    }
+
+    public Resource(String name, Money customHourlyRate) {
+        this.name = name;
+        this.customHourlyRate = customHourlyRate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Money getCustomHourlyRate() {
+        return customHourlyRate;
+    }
+
+    protected abstract Money baseRatePerHour();
+
+    public abstract String describe();
+
+    public Money hourlyRate() {
+        return customHourlyRate != null? customHourlyRate : baseRatePerHour();
+    }
+}
