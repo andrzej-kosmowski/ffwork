870da4dcd3aa7a668ff8bc6fc90052402b2b5a03
Andrzej <bazylek11@gmail.com>
2026-04-23 17:22:45 +0200

    feat: Invoice record and Billable interface implemented

---
+++ src/domain/invoice/Billable.java
@@ -0,0 +1,7 @@
+package domain.invoice;
+
+import domain.booking.Booking;
+
+public interface Billable {
+    Invoice toInvoice(Booking booking);
+}
---
+++ src/domain/invoice/Invoice.java
@@ -0,0 +1,20 @@
+package domain.invoice;
+
+import domain.user.User;
+import money.Money;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public record Invoice(String invoiceNumber, LocalDateTime issueDate, User buyer, Money total, String itemDescription) {
+
+    @Override
+    public String toString() {
+        return String.format("Invoice %s%mDate: %s%nBuyer: %s%nAmount: %s%nDescription: %s",
+                invoiceNumber,
+                issueDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")),
+                buyer,
+                total,
+                itemDescription);
+    }
+}
