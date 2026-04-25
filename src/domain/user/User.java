3a482650f7cb08fc341263a6928b9d908031382d
Andrzej <bazylek11@gmail.com>
2026-04-25 15:19:40 +0200

    feat: walletPayment implemented

--- src/domain/user/User.java
+++ src/domain/user/User.java
@@ -3,14 +3,18 @@ package domain.user;
 import discount.Discount;
 import discount.Discountable;
 import discount.NoDiscount;
+import money.Money;
+import payment.Wallet;
 
 public abstract class User implements Discountable {
     private final String email;
     private final String displayName;
+    private final Wallet wallet;
 
     public User(String email, String displayName) {
         this.email = email;
         this.displayName = displayName;
+        this.wallet = new Wallet(Money.of("0.00"));
     }
 
     public String getEmail() {
@@ -19,6 +23,10 @@ public abstract class User implements Discountable {
 
     public String getDisplayName() {
         return displayName;
+    }
+
+    public Wallet getWallet() {
+        return wallet;
     }
 
     @Override
