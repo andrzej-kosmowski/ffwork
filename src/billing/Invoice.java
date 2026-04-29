29f588a7a4e94e704046ae6c830ab75fec0b32d0
Andrzej <bazylek11@gmail.com>
2026-04-29 18:50:07 +0200

    refactor extract DateTimeFormatter to static constatnt in Invoice

--- src/billing/Invoice.java
+++ src/billing/Invoice.java
@@ -7,12 +7,13 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public record Invoice(String invoiceNumber, LocalDateTime issueDate, User buyer, Money total, String itemDescription) {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     @Override
     public String toString() {
         return String.format("Invoice %s%nDate: %s%nBuyer: %s%nAmount: %s%nDescription: %s",
                 invoiceNumber,
-                issueDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")),
+                issueDate.format(FORMATTER),
                 buyer,
                 total,
                 itemDescription);
