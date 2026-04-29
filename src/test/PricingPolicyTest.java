73afd7e3e1edc221508e6ea3275f2a9858e2dca7
Andrzej <bazylek11@gmail.com>
2026-04-29 19:04:24 +0200

    test: add StandardPricing and Pricing + Discount composition tests

--- src/test/PricingPolicyTest.java
+++ src/test/PricingPolicyTest.java
@@ -5,7 +5,7 @@ import domain.booking.BookingStatus;
 import domain.resource.Desk;
 import domain.resource.DeskType;
 import domain.resource.Resource;
-import domain.resource.Room;
+import domain.user.CompanyUser;
 import domain.user.IndividualUser;
 import domain.user.User;
 import money.Money;
@@ -14,11 +14,9 @@ import pricing.HappyHoursPricing;
 import pricing.PricingPolicy;
 import pricing.StandardPricing;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PricingPolicyTest {
     private final HappyHoursPricing pricing = new HappyHoursPricing();
@@ -70,5 +68,53 @@ public class PricingPolicyTest {
         Booking booking = createBooking(15, 17);
         Money price = pricing.price(booking);
         assertEquals(Money.of("102.00"), price);
+    }
+
+    @Test
+    void shouldCalculateStandardPriceCorrectly() {
+        PricingPolicy pricing = new StandardPricing();
+        Booking booking = createBooking(10, 12);
+        Money price = pricing.price(booking);
+
+        assertEquals(Money.of("120.00"), price);
+    }
+
+    @Test
+    void shouldApplyStudentDiscountOnStandardPricing() {
+        PricingPolicy pricing = new StandardPricing();
+        User user = new IndividualUser("test@mail.com", "Test", "123");
+        Resource resource = new Desk("D1", Money.of("60.00"), DeskType.HOT);
+
+        Booking booking = new Booking(
+                "2",
+                user,
+                resource,
+                LocalDateTime.of(2026,4 ,4, 14, 0),
+                LocalDateTime.of(2026,4 ,4, 16, 0),
+                BookingStatus.PENDING,
+                null
+        );
+
+        Money studentPrice = pricing.price(booking);
+        assertEquals(Money.of("96.00"), studentPrice);
+    }
+
+    @Test
+    void shouldApplyCompanyTierDiscountOnHappyHoursPricing() {
+        User user = new CompanyUser("test@mail.com", "Test", "Test", "123", 1);
+        Resource resource = new Desk("D1", Money.of("60.00"), DeskType.HOT);
+
+        Booking booking = new Booking(
+                "3",
+                user,
+                resource,
+                LocalDateTime.of(2026,4 ,4, 14, 0),
+                LocalDateTime.of(2026,4 ,4, 16, 0),
+                BookingStatus.PENDING,
+                null
+        );
+
+        Money studentPrice = pricing.price(booking);
+        assertEquals(Money.of("79.80"), studentPrice);
     }
 }
